082e3e6278d2dac65721cfb563fd0563b3a0e6d6
Sam Tyler <sam@sammtyler.com>
2026-02-25 13:24:05 +1030

    Increase GitHub theme priority
    This means that it will be loaded before Lazy so the Lazy UI will
    have the theme applied.

--- lua/plugins/github-color-scheme.lua
+++ lua/plugins/github-color-scheme.lua
@@ -2,6 +2,7 @@ return {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
     lazy = false,
+    priority = 100,
     config = function()
         if vim.g.neovide then
             require("github-theme").setup()
