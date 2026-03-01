b586a546a138eeae69ae62c14bb1c697c4d811da
Sam Tyler <sam@sammtyler.com>
2026-03-01 17:22:03 +1030

    Set up autocompletion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
-    init = function()
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+        vim.lsp.config("ts_ls", {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        })
         vim.lsp.enable("ts_ls")
     end,
 }
